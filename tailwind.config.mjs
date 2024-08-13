f8e3ac9576400eda2a53ce6b53016ac616cff9d4
Alipori PC <alipori.bot@gmail.com>
2024-08-13 16:34:58 -0600

    Finalizado la primera version de la pagina

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,23 @@ export default {
 	theme: {
 		extend: {
 			
-
+			keyframes: {
+				fadeIn: {
+				  '0%': { opacity: '0' },
+				  '100%': { opacity: '1' },
+				},
+				slideIn: {
+				  '0%': { transform: 'translateY(100%)' },
+				  '100%': { transform: 'translateY(0)' },
+				},
+			  },
+			  animation: {
+				fadeIn: 'fadeIn 2s ease-in-out',
+				slideIn: 'slideIn 1s ease-out',
+			  },
 		},
 	},
 	plugins: [],
+	darkMode: false, // Desactiva el modo oscuro
+
 }
